56915ce38fbacac7a4f6d3572509c6c4cc4f7e65
Imran-Ali-Khokhar <ali.khokhar50@gmail.com>
2021-12-25 23:18:50 +0500

    added handler function for checking unchecking

--- src/App.js
+++ src/App.js
@@ -3,15 +3,40 @@ import Header from './components/Header'
 import Todolist from './components/Todolist'
 import todosData from './data/todosData'
 
-function App() {
-  const todoComponents = todosData.map(item => <Todolist key={item.id} item={item} />)
+class App extends React.Component {
 
-  return (
-    <div>
+  constructor(){
+    super()
+    this.state = {
+      todos: todosData
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id){
+    this.setState(prevState => {
+        const updatedTodos = prevState.todos.map(todo => {
+        const changedTodo = Object.assign({}, todo, {
+        completed: todo.id === id ? !todo.completed : todo.completed
+        });
+        return changedTodo;
+        })
+        return{
+          todos: updatedTodos
+        }
+    })
+  }
+
+  render(){
+    const todoItems = this.state.todos.map(item => <Todolist key={item.id} item={item} handleChange={this.handleChange} />)
+    return (
+      <div>
         <Header />
-        {todoComponents}
-    </div>
-  );
+        {todoItems}
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -3,8 +3,11 @@ import React from 'react'
 function Todolist(props){
 	return(
 		<div className='todo-item'>
-			<input type='checkbox' checked={props.item.completed} />
-			<p>{props.item.text}</p>
+			<input type='checkbox'
+				   checked={props.item.completed} 
+				   onChange={() => props.handleChange(props.item.id)} 
+			/>
+			<span>{props.item.text}</span>
 		</div>
 		)
 }
